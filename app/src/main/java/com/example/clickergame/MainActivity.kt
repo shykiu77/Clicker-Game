576247692f178bcb73864a574ceecfb3dd961116
Carlos Gabriel Santos de Melo <carlosgabrielsns@gmail.com>
2021-02-11 12:09:56 -0300

    Added toolbar layout.

---
+++ app/src/main/java/com/example/clickergame/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.clickergame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.main_toolbar))
+        supportActionBar?.setDisplayShowTitleEnabled(false);
+    }
+}
